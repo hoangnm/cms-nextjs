f792e14c5d1e07e04f18dd032c2a4566c3251d56
hoangnm <nguyenminhhoang210891@gmail.com>
2020-02-05 15:07:59 +0700

    init create post page

---
+++ pages/create.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
