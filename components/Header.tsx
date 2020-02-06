338188d435433ef69ea8f76f529373b4637f50fd
hoangnm <nguyenminhhoang210891@gmail.com>
2020-02-06 22:21:28 +0700

    fix lint on Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { withRouter } from "next/router";
 import Link from "next/link";
 
 const Header = ({ router }) => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div className="container">
-      <a className="navbar-brand" href="#">
-        Blog
-      </a>
+      <Link href="/">
+        <a className="navbar-brand">Blog</a>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -16,12 +17,12 @@ const Header = ({ router }) => (
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarResponsive">
         <ul className="navbar-nav ml-auto">
           <li className={`nav-item ${router.pathname === "/" ? "active" : ""}`}>
-            <Link href={"/"}>
+            <Link href="/">
               <a className="nav-link">
                 Home
                 <span className="sr-only">(current)</span>
@@ -33,7 +34,7 @@ const Header = ({ router }) => (
               router.pathname === "/create" ? "active" : ""
             }`}
           >
-            <Link href={"/create"}>
+            <Link href="/create">
               <a className="nav-link">Create</a>
             </Link>
           </li>
